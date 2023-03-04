717cc78ddcc52272eaccbd41abd3a2e456606cfb
Nona <111157914+Nonatsikarishvili@users.noreply.github.com>
2023-03-04 07:07:33 -0500

    add file

---
+++ scrpt.js
@@ -0,0 +1,81 @@
+"use strict";
+
+// davaleba #1
+// let array = [5, 25, 89, 120, 36];
+
+// array.push("javascript", "python");
+// array.unshift("html", "css");
+// for (let index in array) {
+//     console.log(index);
+//   }
+// array.shift()
+// array.pop()
+// console.log(array);
+
+// davaleba #2
+// let fruits = ["ფორთოხალი", "ბანანი", "მსხალი"];
+// for (let i in fruits) {
+// //   console.log(i);
+// }
+// fruits.push("ვაშლი", 'ანანასი');
+// fruits.unshift('საზამთრო')
+// // console.log(fruits);
+// fruits.splice(2, 0, 'მანგო')
+// // console.log(fruits);
+// fruits.pop();
+// fruits.shift();
+// console.log(fruits)
+
+// davaleba #3
+// let array = [1, 2, 3, 4, 5];
+// array.splice(3, 0, "a", "b", "c");
+// console.log(array);
+
+// davaleba #4
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// arr.forEach(function (x) {
+//   sum += x;
+// });
+// console.log(sum);
+
+// davaleba #5
+// for (let i = 100; i > 0; i--){
+//     console.log(i);
+// }
+
+// davaleba #6
+// decloration:
+// function test(x, y) {
+//   if (x > y) {
+//     return "num1 is the largest";
+//   } else if (x < y) {
+//     return "num2 is the largest";
+//   }
+//   {
+//     return "error";
+//   }
+// }
+// let result=test(10, 50);
+// console.log(result);
+
+// expression
+
+// let func = function (x, y) {
+//     let i = (x > y) ? "num1 is the largest" :
+//         (x < y) ? "num2 is the largest" :
+//             'error';
+
+//     console.log(i);
+
+// }
+// func(20, 20)
+
+// arrow
+
+// let testfunc = (x, y) => {
+//   let index =
+//     x > y ? "num1 is the largest" : x < y ? "num2 is the largest" : "error";
+//   console.log(index);
+// };
+// testfunc(40, 20);
